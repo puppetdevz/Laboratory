dfb7c30fe544b773b5b003fdddaaae3de0d3f4cf
puppet <puppetdev@163.com>
2022-12-29 23:50:17 +0800

    Auto Pushed By Python Scripts at 2022-12-29 23:50:17

--- demo-spring/demo16-spel/src/test/java/top/puppetdev/demo/SpelTest.java
+++ demo-spring/demo16-spel/src/test/java/top/puppetdev/demo/SpelTest.java
@@ -2,6 +2,7 @@ package top.puppetdev.demo;
 
 import org.junit.Test;
 import org.springframework.expression.Expression;
+import org.springframework.expression.ParserContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
@@ -17,5 +18,50 @@ public class SpelTest {
         StandardEvaluationContext context = new StandardEvaluationContext();
         context.setVariable("end", "!");
         System.out.println(expression.getValue(context));
+    }
+
+    @Test
+    public void testParserContext() {
+        SpelExpressionParser parser = new SpelExpressionParser();
+        ParserContext parserContext = new ParserContext() {
+            @Override
+            public boolean isTemplate() {
+                return true;
+            }
+
+            @Override
+            public String getExpressionPrefix() {
+                return "#{";
+            }
+
+            @Override
+            public String getExpressionSuffix() {
+                return "}";
+            }
+        };
+        String template = "#{'hello '}#{'world!'}";
+        Expression expression = parser.parseExpression(template, parserContext);
+        System.out.println(expression.getValue());
+    }
+
+    @Test
+    public void testClassTypeExpression() {
+        SpelExpressionParser parser = new SpelExpressionParser();
+        // java.lang 包下的类访问
+        Class<String> result1 = parser.parseExpression("T(String)").getValue(Class.class);
+        System.out.println(result1);
+
+        // 其他包类访问
+        String exp = "T(top.puppetdev.demo.SpelTest)";
+        Class<SpelTest> result2 = parser.parseExpression(exp).getValue(Class.class);
+        System.out.println(result2 == SpelTest.class);
+
+        // 类静态字段访问
+        int result3 = parser.parseExpression("T(Integer).MAX_VALUE").getValue(int.class);
+        System.out.println(result3 == Integer.MAX_VALUE);
+
+        // 类静态方法调用
+        int result4 = parser.parseExpression("T(Integer).parseInt('1')").getValue(int.class);
+        System.out.println(result4);
     }
 }
